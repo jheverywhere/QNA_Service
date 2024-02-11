048db052e88f250dae3ca3f6e730eb153888e22c
jheverywhere <nighteolt@naver.com>
2024-02-11 22:22:14 +0900

    GetMapping, PostMapping, RequestMapping의 차이 설명

--- src/main/java/com/exam/jheverywhere/controller/MainController.java
+++ src/main/java/com/exam/jheverywhere/controller/MainController.java
@@ -1,8 +1,7 @@
 package com.exam.jheverywhere.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
@@ -10,7 +9,41 @@ public class MainController {
   // 아래 함수의 리턴값을 그대로 브라우저에 표시
   // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답을 바디에 담는다.
   @ResponseBody
-  public String index(){
-    return "제발 되라 왜 안되는거야 ㅠㅠ";
+  public String index() {
+    return "반갑습니다";
   }
+
+  @GetMapping("/page1")
+  @ResponseBody
+  public String showGet() {
+    return """
+        <form method="POST" action="/page2"/>
+          <input type="number" name="age" placeholder="나이 입력" value="page2로 POST 방식으로 이동" />
+          <input type="submit" value="page2로 POST 방식으로 이동" />
+        </form>
+        """;
+  }
+  @PostMapping("/page2")
+  @ResponseBody
+  public String showPage2Post(@RequestParam(defaultValue="0") int age) {
+    return """
+        <h1>입력된 나이 : %d</h1>
+        <h1>안녕하세요. POST 방식으로 오신걸 환영합니다.</h1>
+        """.formatted(age);
+  }
+
+
+
+  @GetMapping("/page2")
+  @ResponseBody
+  public String showPost(@RequestParam(defaultValue="0")int age) {
+    return """
+        <h1>입력된 나이 : %d</h1>
+        <h1>안녕하세요. GET 방식으로 오신걸 환영합니다.</h1>
+
+        """.formatted(age);
+  }
+
+
 }
+
