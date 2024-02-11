d88b3576624b8dab798579f91f8771f4e7d3e1a7
jheverywhere <nighteolt@naver.com>
2024-02-11 17:17:39 +0900

    세팅

---
+++ src/main/java/com/exam/jheverywhere/JheverywhereApplication.java
@@ -0,0 +1,13 @@
+package com.exam.jheverywhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JheverywhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JheverywhereApplication.class, args);
+	}
+
+}
